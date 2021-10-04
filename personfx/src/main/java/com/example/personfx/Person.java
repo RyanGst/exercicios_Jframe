be21a6b7124aa49a4c526a09775081d2ddafecbb
RyanGst <ryangnr123@outlook.com>
2021-10-04 16:27:55 -0300

    first commit

---
+++ personfx/src/main/java/com/example/personfx/Person.java
@@ -0,0 +1,35 @@
+package com.example.personfx;
+
+public class Person {
+
+    String nome;
+    Float altura;
+    String birthDate;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Float getAltura() {
+        return altura;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAltura(Float altura) {
+        this.altura = altura;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+}
+
